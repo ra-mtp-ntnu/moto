079494689e685d91c2a2f1b96add959f0e1af565
Lars Tingelstad <lars.tingelstad@ntnu.no>
2020-10-16 15:58:06 +0200

    Started telnet impl for debugging

---
+++ moto/telnet.py
@@ -0,0 +1,25 @@
+from telnetlib import Telnet
+from threading import Thread, Lock
+import logging
+
+
+class TelnetConnection:
+    def __init__(self, ip_address: str):
+        self._ip_address: str = ip_address
+        self._username: str = "MOTOMANrobot"
+        self._password: str = "MOTOMANrobot"
+        self._tn: Telnet = Telnet(self._ip_address)
+
+        self._run_thread = Thread(target=self._run)
+        self._run_thread.daemon = True
+
+    def login(self) -> None:
+        self._tn.read_until(b"login: ")
+        self._tn.write(self._username.encode("ascii") + b"\n")
+        self._tn.read_until(b"Password: ")
+        self._tn.write(self._password.encode("ascii") + b"\n")
+
+        self._run_thread.start()
+
+    def _run(self) -> None:
+        pass
