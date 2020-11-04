fad88aabab6249733eec806a20151a673f24273c
Lars Tingelstad <lars.tingelstad@ntnu.no>
2020-11-04 13:41:33 +0000

    testing with two control groups

--- test/test_moto.py
+++ test/test_moto.py
@@ -7,72 +7,25 @@ import copy
 
 
 # m = Moto("192.168.255.200", [("R1", 6)])
-m = Moto("localhost", [("R1", 6)])
+m = Moto("localhost", [("R1", 6), ("S1", 2)]) # Sim with two groups
 
 r = m.control_groups["R1"]
 
 
-def send_pts():
-    x= copy.deepcopy(list(r.joint_feedback.pos))
-    pt1 = JointTrajPtFull(0, 0, int("1111",2), 0.0, copy.deepcopy(x), [0.0]*10, [0.0]*10)
-    x[0] -= np.deg2rad(30)
-    pt2 = JointTrajPtFull(0, 1, int("1111",2), 10.0, copy.deepcopy(x), [0.0]*10, [0.0]*10)
-    x[0] += np.deg2rad(5)
-    pt3 = JointTrajPtFull(0, 2, int("1111",2), 20.0, copy.deepcopy(x), [0.0]*10, [0.0]*10)
+def send_pts(seq, p, t):
+    x = copy.deepcopy(list(r.joint_feedback.pos))
+    x[0] = np.deg2rad(p)
+    pt1 = JointTrajPtFull(
+        0, 2, int("1111", 2), t, copy.deepcopy(x), [0.0] * 10, [0.0] * 10
+    )
 
-    r.send_joint_traj_pt_full(pt1)
-    r.send_joint_traj_pt_full(pt2)
-    r.send_joint_traj_pt_full(pt3)
+    # m._motion_connection.send_joint_traj_pt_full(pt1)
+    # m._motion_connection.send_joint_traj_pt_full(pt2)
+    m._motion_connection.send_joint_traj_pt_full(pt1)
+    time.sleep(0.01)
 
+time.sleep(1.0)
 
-
-# print(r.groupid)
-
-# pt1 = JointTrajPtFull(0, 0, int("1111", 2), 10.0, [1.0] * 10, [0.0] * 10, [0.0] * 10)
-
-# pt2 = JointTrajPtFull(0, 0, int("1111", 2), 20.0, [2.0] * 10, [0.0] * 10, [0.0] * 10)
-
-# r.send_joint_traj_pt_full(pt1)
-# r.send_joint_traj_pt_full(pt2)
-
-
-# for _ in range(1000):
-#     time.sleep(0.1)
-#     print(r.joint_feedback)
-
-
-# robot = m.control_group(0)
-
-# # robot = cell.control_group("R1")
-# # positioner = cell.control_group[1]
-
-# pos = np.array([ 0.00438486, -0.82990497, -0.89975643, -0.59869617, -0.96005285,
-#        -0.10957382,  0.        ,  0.        ,  0.        ,  0.        ])
-
-# pos2 = pos.copy()
-
-# pos2[0] += np.deg2rad(5)
-
-
-# def sendpt(pos, time, sequence):
-#     vel = np.zeros(10)
-#     acc = np.zeros(10)
-
-#     body  = JointTrajPtFull(0, sequence, int("1111",2), time, pos, vel, acc )
-
-#     header = Header(MsgType.JOINT_TRAJ_PT_FULL, CommType.TOPIC, ReplyType.INVALID)
-
-#     msg = header.to_bytes() + body.to_bytes()
-#     msg = Prefix(len(msg)).to_bytes() + msg
-
-#     robot.motion_connection._tcp_client.send(msg)
-
-# servos_started = robot.motion_connection.start_servos()
-# print(servos_started)
-
-# motion_ready = robot.motion_connection.check_motion_ready()
-# print(motion_ready)
-
-
-# servos_stopped = robot._motion_connection.stop_servos()
-# print(servos_stopped)
+send_pts(0, 0.0, 0.0)
+send_pts(1, 45.0, 10.0)
+send_pts(2, 20.0, 20.0)
