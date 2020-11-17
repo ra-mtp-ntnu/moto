740e55a9a4bb111a52e42326e2d8e7a2f94dd6d4
Lars Tingelstad <lars.tingelstad@ntnu.no>
2020-11-17 15:41:35 +0100

    test rt motion server sim

--- test/test_rt_motion_server.py
+++ test/test_rt_motion_server.py
@@ -15,29 +15,42 @@ def start_udp_server(address):
 
 def main():
     server = start_udp_server(("localhost", 50244))
+    started = False
     while True:
-        bytes_, addr = server.recvfrom(1024)
 
-        msg = SimpleMessage.from_bytes(bytes_)
-        state: MotoRealTimeMotionJointStateEx = msg.body
+        try:
+            bytes_, addr = server.recvfrom(1024)
+            if not bytes_:
+                logging.error("Stopping!")
+                break
 
-        command_msg: SimpleMessage = SimpleMessage(
-            Header(
-                MsgType.MOTO_REALTIME_MOTION_JOINT_COMMAND_EX,
-                CommType.TOPIC,
-                ReplyType.INVALID,
-            ),
-            MotoRealTimeMotionJointCommandEx(
-                state.message_id,
-                state.number_of_valid_groups,
-                [
-                    MotoRealTimeMotionJointCommandExData(joint_state.groupno, [1.0] * 6)
-                    for joint_state in state.joint_state_data
-                ],
-            ),
-        )
+            if not started:
+                server.settimeout(1.0)
+                started = True
 
-        server.sendto(command_msg.to_bytes(), addr)
+            msg = SimpleMessage.from_bytes(bytes_)
+            state: MotoRealTimeMotionJointStateEx = msg.body
+
+            command_msg: SimpleMessage = SimpleMessage(
+                Header(
+                    MsgType.MOTO_REALTIME_MOTION_JOINT_COMMAND_EX,
+                    CommType.TOPIC,
+                    ReplyType.INVALID,
+                ),
+                MotoRealTimeMotionJointCommandEx(
+                    state.message_id,
+                    state.number_of_valid_groups,
+                    [
+                        MotoRealTimeMotionJointCommandExData(joint_state.groupno, [0.1] * 6)
+                        for joint_state in state.joint_state_data
+                    ],
+                ),
+            )
+
+            server.sendto(command_msg.to_bytes(), addr)
+        except socket.timeout as e:
+            logging.error("Timed out!")
+            break
 
 
 if __name__ == "__main__":
