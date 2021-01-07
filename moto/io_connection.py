10947e245e6619475ae682e90cd7a7ab92bb7392
Lars Tingelstad <lars.tingelstad@ntnu.no>
2021-01-07 15:24:43 +0100

    rename send and recv request method

--- moto/io_connection.py
+++ moto/io_connection.py
@@ -30,7 +30,7 @@ class IoConnection(SimpleMessageConnection):
     def __init__(self, ip_address):
         super().__init__((ip_address, self.TCP_PORT_IO))
 
-    def _send_and_recv_io_request(self, msg_type: MsgType, *args: int) -> SimpleMessage:
+    def _send_and_recv_request(self, msg_type: MsgType, *args: int) -> SimpleMessage:
         request: SimpleMessage = SimpleMessage(
             Header(msg_type, CommType.SERVICE_REQUEST, ReplyType.INVALID),
             MSG_TYPE_CLS[msg_type](*args),
@@ -42,15 +42,15 @@ class IoConnection(SimpleMessageConnection):
         self._tcp_client.connect()
 
     def read_io_bit(self, address: int):
-        return self._send_and_recv_io_request(MsgType.MOTO_READ_IO_BIT, address)
+        return self._send_and_recv_request(MsgType.MOTO_READ_IO_BIT, address)
 
     def write_io_bit(self, address: int, value: int):
-        return self._send_and_recv_io_request(MsgType.MOTO_WRITE_IO_BIT, address, value)
+        return self._send_and_recv_request(MsgType.MOTO_WRITE_IO_BIT, address, value)
 
     def read_io_group(self, address: int):
-        return self._send_and_recv_io_request(MsgType.MOTO_READ_IO_GROUP, address)
+        return self._send_and_recv_request(MsgType.MOTO_READ_IO_GROUP, address)
 
     def write_io_group(self, address: int, value: int):
-        return self._send_and_recv_io_request(
+        return self._send_and_recv_request(
             MsgType.MOTO_WRITE_IO_GROUP, address, value
         )
--- moto/real_time_motion_connection.py
+++ moto/real_time_motion_connection.py
@@ -13,6 +13,15 @@
 # limitations under the License.
 
 from moto.simple_message_connection import SimpleMessageConnection
+from moto.simple_message import (
+    Header,
+    MsgType,
+    CommType,
+    ReplyType,
+    MotoMotionCtrl,
+    CommandType,
+    SimpleMessage,
+)
 
 
 class RealTimeMotionConnection(SimpleMessageConnection):
@@ -21,3 +30,23 @@ class RealTimeMotionConnection(SimpleMessageConnection):
 
     def __init__(self, ip_address):
         super().__init__((ip_address, self.TCP_PORT_REALTIME_MOTION))
+
+    def _send_and_recv_request(
+        self, command: CommandType, groupno=-1
+    ) -> SimpleMessage:
+        request = SimpleMessage(
+            Header(
+                MsgType.MOTO_MOTION_CTRL, CommType.SERVICE_REQUEST, ReplyType.INVALID
+            ),
+            MotoMotionCtrl(groupno, -1, command),
+        )
+        response = self.send_and_recv(request)
+        return response
+
+    def start_rt_mode(self):
+        return self._send_and_recv_request(CommandType.START_REALTIME_MOTION_MODE)
+
+    def stop_rt_mode(self):
+        return self._send_and_recv_request(CommandType.STOP_REALTIME_MOTION_MODE)
+
+
