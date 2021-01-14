4ea12b98e278ef8c96e8a93e2a04e5dfbb554e73
Lars Tingelstad <lars.tingelstad@ntnu.no>
2021-01-14 13:09:00 +0100

    Added unittests for RobotStatus, JointFeedback, and MotoMotionCtrl

--- test/test_simple_message.py
+++ test/test_simple_message.py
@@ -1,5 +1,42 @@
 import unittest
-from moto.simple_message import JointTrajPtFull, ValidFields
+
+from moto.simple_message import (
+    JointFeedback,
+    JointTrajPtFull,
+    MotoMotionCtrl,
+    PendantMode,
+    RobotStatus,
+    CommandType,
+    Ternary,
+    ValidFields,
+)
+
+
+class TestRobotStatus(unittest.TestCase):
+    def test_to_and_from_bytes(self):
+        robot_status = RobotStatus(
+            drives_powered=Ternary.TRUE,
+            e_stopped=Ternary.TRUE,
+            error_code=42,
+            in_error=Ternary.TRUE,
+            in_motion=Ternary.TRUE,
+            mode=PendantMode.MANUAL,
+            motion_possible=Ternary.TRUE,
+        )
+
+        robot_status_from_bytes = RobotStatus.from_bytes(robot_status.to_bytes())
+
+        self.assertEqual(
+            robot_status.drives_powered, robot_status_from_bytes.drives_powered
+        )
+        self.assertEqual(robot_status.e_stopped, robot_status_from_bytes.e_stopped)
+        self.assertEqual(robot_status.error_code, robot_status_from_bytes.error_code)
+        self.assertEqual(robot_status.in_error, robot_status_from_bytes.in_error)
+        self.assertEqual(robot_status.in_motion, robot_status_from_bytes.in_motion)
+        self.assertEqual(robot_status.mode, robot_status_from_bytes.mode)
+        self.assertEqual(
+            robot_status.motion_possible, robot_status_from_bytes.motion_possible
+        )
 
 
 class TestJointTrajPtFull(unittest.TestCase):
@@ -47,6 +84,59 @@ class TestJointTrajPtFull(unittest.TestCase):
             joint_traj_pt_full.acc, joint_traj_pt_full_from_bytes.acc
         ):
             self.assertAlmostEqual(a, a_from_bytes)
+
+
+class TestJointFeedback(unittest.TestCase):
+    def test_to_and_from_bytes(self):
+        joint_feedback = JointFeedback(
+            groupno=0,
+            valid_fields=ValidFields.TIME
+            | ValidFields.POSITION
+            | ValidFields.VELOCITY
+            | ValidFields.ACCELERATION,
+            time=42.1,
+            pos=[0.0] * 10,
+            vel=[0.0] * 10,
+            acc=[0.0] * 10,
+        )
+
+        joint_feedback_from_bytes = JointFeedback.from_bytes(joint_feedback.to_bytes())
+
+        self.assertEqual(joint_feedback.groupno, joint_feedback_from_bytes.groupno)
+        self.assertEqual(
+            joint_feedback.valid_fields, joint_feedback_from_bytes.valid_fields
+        )
+        self.assertAlmostEqual(
+            joint_feedback.time, joint_feedback_from_bytes.time, places=4
+        )
+
+        for p, p_from_bytes in zip(joint_feedback.pos, joint_feedback_from_bytes.pos):
+            self.assertAlmostEqual(p, p_from_bytes)
+
+        for v, v_from_bytes in zip(joint_feedback.vel, joint_feedback_from_bytes.vel):
+            self.assertAlmostEqual(v, v_from_bytes)
+
+        for a, a_from_bytes in zip(joint_feedback.acc, joint_feedback_from_bytes.acc):
+            self.assertAlmostEqual(a, a_from_bytes)
+
+
+class TestMotoMotionCtrl(unittest.TestCase):
+    def test_to_and_from_bytes(self):
+        moto_motion_ctrl = MotoMotionCtrl(
+            groupno=1, sequence=42, command=CommandType.START_SERVOS, data=[0.0] * 10
+        )
+
+        moto_motion_ctrl_from_bytes = MotoMotionCtrl.from_bytes(
+            moto_motion_ctrl.to_bytes()
+        )
+
+        self.assertEqual(moto_motion_ctrl.groupno, moto_motion_ctrl_from_bytes.groupno)
+        self.assertEqual(
+            moto_motion_ctrl.sequence, moto_motion_ctrl_from_bytes.sequence
+        )
+        self.assertEqual(moto_motion_ctrl.command, moto_motion_ctrl_from_bytes.command)
+        for d, d_from_bytes in zip(moto_motion_ctrl.data, moto_motion_ctrl.data):
+            self.assertAlmostEqual(d, d_from_bytes)
 
 
 if __name__ == "__main__":
