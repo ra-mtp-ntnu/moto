bd485750ae032d09d59de960d3fb8a2db800d1bf
Lars Tingelstad <lars.tingelstad@ntnu.no>
2021-01-19 12:06:08 +0100

    Added ex traj point

--- examples/test_moto.py
+++ examples/test_moto.py
@@ -1,13 +1,19 @@
 from moto import motion_connection
 from moto import Moto, ControlGroupDefinition
-from moto.simple_message import JointTrajPtFull
+from moto.simple_message import (
+    JointFeedbackEx,
+    JointTrajPtExData,
+    JointTrajPtFull,
+    JointTrajPtFullEx,
+    ValidFields,
+)
 
 import time
 import numpy as np
 import copy
 
 m = Moto(
-    "localhost",
+    "192.168.255.200",
     [
         ControlGroupDefinition(
             groupid="robot",
@@ -26,34 +32,65 @@ m = Moto(
             groupid="positioner",
             groupno=1,
             num_joints=2,
-            joint_names=[
-                "joint_1",
-                "joint_2",
-            ],
+            joint_names=["joint_1", "joint_2",],
+        ),
+    ],
+    start_motion_connection=True,
+    start_state_connection=True,
+)
+
+
+robot = m.control_groups["robot"]
+positioner = m.control_groups["positioner"]
+
+time.sleep(1)
+
+
+robot_joint_feedback: JointFeedbackEx = m.state.joint_feedback_ex()
+
+p0 = JointTrajPtFullEx(
+    number_of_valid_groups=2,
+    sequence=0,
+    joint_traj_pt_data=[
+        JointTrajPtExData(
+            groupno=0,
+            valid_fields=ValidFields.TIME | ValidFields.POSITION | ValidFields.VELOCITY,
+            time=0.0,
+            pos=robot_joint_feedback.joint_feedback_data[0].pos,
+            vel=[0.0] * 10,
+            acc=[0.0] * 10,
+        ),
+        JointTrajPtExData(
+            groupno=1,
+            valid_fields=ValidFields.TIME | ValidFields.POSITION | ValidFields.VELOCITY,
+            time=0.0,
+            pos=robot_joint_feedback.joint_feedback_data[1].pos,
+            vel=[0.0] * 10,
+            acc=[0.0] * 10,
         ),
     ],
 )
 
+p1 = JointTrajPtFullEx(
+    number_of_valid_groups=2,
+    sequence=1,
+    joint_traj_pt_data=[
+        JointTrajPtExData(
+            groupno=0,
+            valid_fields=ValidFields.TIME | ValidFields.POSITION | ValidFields.VELOCITY,
+            time=5.0,
+            pos=np.deg2rad([10.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]),
+            vel=[0.0] * 10,
+            acc=[0.0] * 10,
+        ),
+        JointTrajPtExData(
+            groupno=1,
+            valid_fields=ValidFields.TIME | ValidFields.POSITION | ValidFields.VELOCITY,
+            time=10.0,
+            pos=np.deg2rad([0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]),
+            vel=[0.0] * 10,
+            acc=[0.0] * 10,
+        ),
+    ],
+)
 
-r = m.control_groups["robot"]
-# s = m.control_groups["S1"]
-
-
-def send_pts(seq, p, t):
-    x = copy.deepcopy(list(r.joint_feedback.pos))
-    x[0] += np.deg2rad(p)
-    pt1 = JointTrajPtFull(
-        0, seq, int("1111", 2), t, copy.deepcopy(x), [0.0] * 10, [0.0] * 10
-    )
-
-    # m._motion_connection.send_joint_traj_pt_full(pt1)
-    # m._motion_connection.send_joint_traj_pt_full(pt2)
-    m._motion_connection.send_joint_traj_pt_full(pt1)
-    time.sleep(0.01)
-
-
-# time.sleep(1.0)
-
-# send_pts(0, 0.0, 0.0)
-# send_pts(1, 45.0, 10.0)
-# send_pts(2, 20.0, 20.0)
